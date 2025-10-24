37e07c6133078acea48b3bb3bcef85faa34201c7
akash <aayush45r@gmail.com>
2025-10-24 18:41:55 +0530

    used field DI inside a class

--- Di/src/main/java/com/akash/di/First.java
+++ Di/src/main/java/com/akash/di/First.java
@@ -1,10 +1,35 @@
 package com.akash.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class First {
+    
+    @Autowired//Field injection!
+    Laptop laptop;
+
     public void build(){
+        laptop.complie();
         System.out.println("Working on awesome project!!");
+    }
+}
+
+@Component
+class Laptop{
+    public void complie(){
+        System.out.println("compliing 404 errors!!");
+    }
+}
+
+interface Student{
+    void name();
+}
+
+@Component
+class Art implements Student{
+    @Override
+    public void name(){
+        System.out.println("My name is akash!!");
     }
 }
