9c9a3dfa2bd6ccad63929e5775c63f5ea4f84931
akash <aayush45r@gmail.com>
2025-10-24 17:43:43 +0530

    Used dependency injection using spring boot

--- Di/src/main/java/com/akash/di/DiApplication.java
+++ Di/src/main/java/com/akash/di/DiApplication.java
@@ -2,12 +2,18 @@ package com.akash.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiApplication.class, args);
+		
+		ApplicationContext context = SpringApplication.run(DiApplication.class, args);
+		
+		First first = context.getBean(First.class);
+		first.build();
+
 	}
 
 }
---
+++ Di/src/main/java/com/akash/di/First.java
@@ -0,0 +1,10 @@
+package com.akash.di;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class First {
+    public void build(){
+        System.out.println("Working on awesome project!!");
+    }
+}
