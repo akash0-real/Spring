ca745f9dcef1bbd4302afac78ab05253c82283bd
akash <aayush45r@gmail.com>
2025-10-24 17:31:06 +0530

    aded new config files for DI

---
+++ Di/src/test/java/com/Akash/Di/DiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Akash.Di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
