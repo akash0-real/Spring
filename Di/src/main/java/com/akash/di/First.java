7e304c298a3fc27011fd4de6df5411bd1494c2bc
akash <aayush45r@gmail.com>
2025-10-24 18:55:10 +0530

    used qualifier

--- Di/src/main/java/com/akash/di/First.java
+++ Di/src/main/java/com/akash/di/First.java
@@ -1,6 +1,7 @@
 package com.akash.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class First {
     
     @Autowired//Field injection!
+    @Qualifier("laptop")
     Job laptop;
 
     public void build(){
